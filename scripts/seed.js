54bf05c54d46cce60ff06785c57a83b3c37219ae
AkivaGreen <green.akiva@outlook.com>
2019-11-09 22:48:11 -0500

    Updated Seed file

--- scripts/seed.js
+++ scripts/seed.js
@@ -6,6 +6,7 @@ const eventsCount = process.env.events || 0;
 const usersCount = process.env.users || 0;
 const garagesCount = process.env.garages || 0;
 var connection;
+var pool;
 
 function twoDigits(d){
 	if(0 <= d && d < 10) return "0" + d.toString();
@@ -20,7 +21,8 @@ Date.prototype.toMysqlFormat = function(){
 }
 
 if(production == true){
-	connection = mysql.createConnection({
+	pool = mysql.createPool({
+		connectionLimit: 10,
 		host: 'localhost',
 		user: 'cen4010fal19_g07',
 		password: 'kJDrofNeU6',
@@ -29,7 +31,8 @@ if(production == true){
 	console.log('Connected to Production Database!');
 }
 else{
-	connection = mysql.createConnection({
+	pool = mysql.createPool({
+		connectionLimit: 10,
 		host: 'localhost',
 		user: 'user',
 		password: 'password',
@@ -38,11 +41,35 @@ else{
 	console.log('Connected to Development Database!');
 }
 
-//Seeds the Database with Records
-connection.connect(function(error){
-	if(error) throw error;
-	//Seeds the Database with User Records
-	for(let i = 0; i < usersCount; i++){
+getFaculty = function(){
+	return new Promise(function(resolve, reject){
+		pool.getConnection(function(error, connection){
+				connection.query('SELECT * FROM users WHERE faculty=true', function(err, rows, fields){
+				connection.release();
+				if(err)
+					return reject(err);
+				resolve(rows);
+			});
+		});
+	});
+}
+
+getUsers = function(){
+	return new Promise(function(resolve, reject){
+		pool.getConnection(function(error, connection){
+			connection.query('SELECT * FROM users', function(err, rows, fields){
+				connection.release();
+				if(err)
+					return reject(err);
+				resolve(rows);
+			});
+		});
+	});
+}
+
+//Seed the Database with User Records
+seedUsers = function(i){
+	return new Promise(function(resolve, reject){
 		let residency_values = ['Out-State', 'In-State'];
 		let residency = residency_values[Math.round(Math.random())];
 		let bool_selection_values = ['commuter', 'dormer', 'faculty']
@@ -150,31 +177,37 @@ connection.connect(function(error){
 		let email = faker.internet.email();
 		let firstname = faker.name.firstName();
 		let lastname = faker.name.lastName();
-
 		let user = `INSERT INTO users(residency, ${bool_selection}, building, room_number, address, \
 		phone_number, email, firstname, lastname) VALUE(\"${residency}\", true, \"${building}\", \
 		\"${room_number}\", \"${address}\", \"${phone_number}\", \"${email}\", \"${firstname}\", \"${lastname}\")`;
-
-		connection.query(user, function(err, rows, fields){
-			if(err) throw err;
-			console.log('---------------------------------------------------');
-			console.log(`User Residency for Record ${i+1}: ${residency}`);
-			console.log(`User ${bool_selection} for Record ${i+1}: ${true}`);
-			if(bool_selection == 'dormer'){
-				console.log(`User Building for Record ${i+1}: ${building}`);
-				console.log(`User Room Number for Record ${i+1}: ${room_number}`);
-			}
-			console.log(`User Address for Record ${i+1}: ${address}`);
-			console.log(`User Phone Number for Record ${i+1}: ${phone_number}`);
-			console.log(`User Email for Record ${i+1}: ${email}`);
-			console.log(`User First Name for Record ${i+1}: ${firstname}`);
-			console.log(`User Last Name for Record ${i+1}: ${lastname}`);
-			console.log(`User Record ${i+1} Created!`);
+		pool.getConnection(function(error, connection){
+			if(error)
+				return reject(error);
+			connection.query(user, function(err, rows, fields){
+				connection.release();
+				if(err)
+					return reject(err);
+				console.log('---------------------------------------------------');
+				console.log(`User Residency for Record ${i+1}: ${residency}`);
+				console.log(`User ${bool_selection} for Record ${i+1}: ${true}`);
+				if(bool_selection == 'dormer'){
+					console.log(`User Building for Record ${i+1}: ${building}`);
+					console.log(`User Room Number for Record ${i+1}: ${room_number}`);
+				}
+				console.log(`User Address for Record ${i+1}: ${address}`);
+				console.log(`User Phone Number for Record ${i+1}: ${phone_number}`);
+				console.log(`User Email for Record ${i+1}: ${email}`);
+				console.log(`User First Name for Record ${i+1}: ${firstname}`);
+				console.log(`User Last Name for Record ${i+1}: ${lastname}`);
+				resolve(rows);
+			});
 		});
-	};
+	});
+}
 
-	//Seeds the Database with Garage Records
-	for(let i = 0; i < garagesCount; i++){
+//Seed the Database with Garage Records
+seedGarages = function(i){
+	return new Promise(function(resolve, reject){
 		let location = `${faker.address.streetAddress()}, Boca Raton, FL 33043`;
 		let name = faker.company.companyName();
 		let full_values = [true, false];
@@ -186,20 +219,27 @@ connection.connect(function(error){
 		let garage = `INSERT INTO garages(location, name, full, free_spots, total_spots) \
 		VALUE(\"${location}\", \"${name}\", ${full}, ${free_spots}, ${total_spots})`;
 
-		connection.query(garage, function(err, rows, fields){
-			if(err) throw err;
-			console.log('---------------------------------------------------');
-			console.log(`Garage Location for Record ${i+1}: ${location}`);
-			console.log(`Garage Name for Record ${i+1}: ${name}`);
-			console.log(`Garage Full Status for Record ${i+1}: ${full}`);
-			console.log(`Garage Free Spots for Record ${i+1}: ${free_spots}`);
-			console.log(`Garage Total Spots for Record ${i+1}: ${total_spots}`);
-			console.log(`Garage Record ${i+1} Created!`);
+		pool.getConnection(function(error, connection){
+			if(error)
+				return reject(error);
+			connection.query(garage, function(err, rows, fields){
+				if(err)
+					return reject(err);
+				console.log('---------------------------------------------------');
+				console.log(`Garage Location for Record ${i+1}: ${location}`);
+				console.log(`Garage Name for Record ${i+1}: ${name}`);
+				console.log(`Garage Full Status for Record ${i+1}: ${full}`);
+				console.log(`Garage Free Spots for Record ${i+1}: ${free_spots}`);
+				console.log(`Garage Total Spots for Record ${i+1}: ${total_spots}`);
+				resolve(rows);
+			});
 		});
-	}
+	});
+}
 
-	//Seeds the Database with Event Records
-	for(let i = 0; i < eventsCount; i++){
+//Seed the Database with Event Records
+seedEvents = function(i){
+	return new Promise(function(resolve, reject){
 		let location = `${faker.lorem.words()} Hall, Boca Campus`;
 		let dates_values = [faker.date.past(), faker.date.recent(), faker.date.future()];
 		let date = dates_values[Math.round(Math.random()*2)];
@@ -211,31 +251,40 @@ connection.connect(function(error){
 		let user_ids = [];
 		let submitted_user;
 		let events;
-		connection.query('SELECT * FROM users WHERE faculty=true', function(err1, rows1, fields1){
-			if(err1) throw err1;
-			for(let j in rows1){
-				user_ids[j] = rows1[j].id;
+
+		getFaculty().then(function(results){
+			for(let i in results){
+				user_ids[i] = results[i].id;
 			}
 			submitted_user = user_ids[Math.round(Math.random()*(user_ids.length-1))];
 			events = `INSERT INTO events(location, start_date, end_date, description, host, submitted_user) \
 			VALUE(\"${location}\", \"${start_date}\", \"${end_date}\", \"${description}\", \"${host}\", ${submitted_user})`;
-
-			connection.query(events, function(err2, rows2, fields2){
-				if(err2) throw err2;
-				console.log('---------------------------------------------------');
-				console.log(`Event Location for Record ${i+1}: ${location}`);
-				console.log(`Event Start Date for Record ${i+1}: ${start_date}`);
-				console.log(`Event End Date for Record ${i+1}: ${end_date}`);
-				console.log(`Event Description for Record ${i+1}: ${description}`);
-				console.log(`Event Host for Record ${i+1}: ${host}`);
-				console.log(`Event Submitted User Id for Record ${i+1}: ${submitted_user}`);
-				console.log(`Event Record ${i+1} Created!`);
-			});		
+			return events;
+		}).then(function(query){
+			pool.getConnection(function(error, connection){
+				if(error)
+					return reject(error);
+				connection.query(query, function(err, rows, fields){
+					connection.release();
+					if(err)
+						return reject(err);
+					console.log('---------------------------------------------------');
+					console.log(`Event Location for Record ${i+1}: ${location}`);
+					console.log(`Event Start Date for Record ${i+1}: ${start_date}`);
+					console.log(`Event End Date for Record ${i+1}: ${end_date}`);
+					console.log(`Event Description for Record ${i+1}: ${description}`);
+					console.log(`Event Host for Record ${i+1}: ${host}`);
+					console.log(`Event Submitted User Id for Record ${i+1}: ${submitted_user}`);
+					resolve(rows);
+				});	
+			});
 		});
-	}
+	});
+}
 
-	//Seeds the Database with Issue Records
-	for(let i = 0; i < issuesCount; i++){
+//Seed the Database with Issue Records
+seedIssues = function(i){
+	return new Promise(function(resolve, reject){
 		let description = faker.lorem.text();
 		let location_values = ['IVAS', 'IVAN', 'UVA 57', 'UVA 58', 'UVA 59', 'UVA 60', 
 		'UVA 61', 'PAR', 'HPT', 'GPT', 'IRT', `${faker.lorem.words()} Hall, Boca Campus`];
@@ -253,59 +302,129 @@ connection.connect(function(error){
 		let verified_faculty = null;
 		let resolved_faculty_values = [];
 		let resolved_faculty = null;
+		let foreign_keys = [];
+		let issues
 
-		connection.query('SELECT * FROM users', function(err1, rows1, fields1){
-			if(err1) throw err1;
-			for(let j in rows1){
-				submitted_user_values[j] = rows1[j].id;
+		getUsers().then(function(users_list){
+			for(let i in users_list){
+				submitted_user_values[i] = users_list[i].id;
 			}
 			submitted_user = submitted_user_values[Math.round(Math.random()*(submitted_user_values.length-1))];
-			if(verified == true){
-				connection.query('SELECT * FROM users WHERE faculty=true', function(err2, rows2, fields2){
-					if(err2) throw err2;
-					for(let k in rows2){
-						verified_faculty_values[k] = rows2[k].id;
-					}
-					verified_faculty = verified_faculty_values[Math.round(Math.random()*(verified_faculty_values.length-1))];
-					if(resolved){
-						connection.query('SELECT * FROM users WHERE faculty=true', function(err3, rows3, fields3){
-							if(err3) throw err3;
-							for(let l in rows3){
-								resolved_faculty_values[l] = rows3[l].id;
-							}
-							resolved_faculty = resolved_faculty_values[Math.round(Math.random()*(resolved_faculty_values.length-1))];
-						});
-					}
-				});
+			issues = `INSERT INTO issues(description, location, verified, resolved, submitted_user, verified_faculty, \
+			resolved_faculty)	VALUE(\"${description}\", \"${location}\", ${verified}, ${resolved}, ${submitted_user},`;
+			return;
+		}).then(function(){
+			return getFaculty();
+		}).then(function(faculty_list){
+			if(verified){ //You have this set to true fix it
+				for(let i in faculty_list){
+					verified_faculty_values[i] = faculty_list[i].id;
+				}
+				verified_faculty = verified_faculty_values[Math.round(Math.random()*(verified_faculty_values.length-1))];
+				issues = issues.concat(` ${verified_faculty},`);
 			}
-			let issues = `INSERT INTO issues(description, location, verified, resolved, submitted_user, verified_faculty \
-			resolved_faculty) VALUE(\"${description}\", \"${location}\", ${verified}, ${resolved}, ${submitted_user}, \
-			${verified_faculty}, ${resolved_faculty})`;
-
-			console.log('---------------------------------------------------');
-			console.log(`Issue Description for Record ${i+1}: ${description}`);
-			console.log(`Issue Location for Record ${i+1}: ${location}`);
-			console.log(`Issue Verified for Record ${i+1}: ${verified}`);
-			console.log(`Issue Resolved for Record ${i+1}: ${resolved}`);
-			console.log(`Issue Submitted User Id for Record ${i+1}: ${submitted_user}`);
-			console.log(`Issue Verified Faculty User Id for Record ${i+1}: ${verified_faculty}`);
-			console.log(`Issue Resolved Faculty User Id for Record ${i+1}: ${resolved_faculty}`);
-			console.log(`Issue Record ${i+1} Created!`);
-			/*
-			connection.query(issues, function(err4, rows4, fields4){
-				if(err4) throw err4;
-				console.log('---------------------------------------------------');
-				console.log(`Issue Description for Record ${i+1}: ${description}`);
-				console.log(`Issue Location for Record ${i+1}: ${location}`);
-				console.log(`Issue Verified for Record ${i+1}: ${verified}`);
-				console.log(`Issue Resolved for Record ${i+1}: ${resolved}`);
-				console.log(`Issue Submitted User Id for Record ${i+1}: ${submitted_user}`);
-				console.log(`Issue Verified Faculty User Id for Record ${i+1}: ${verified_faculty}`);
-				console.log(`Issue Resolved Faculty User Id for Record ${i+1}: ${resolved_faculty}`);
-				console.log(`Issue Record ${i+1} Created!`);
+			else{
+				issues = issues.concat(' null,');
+			}
+			return;
+		}).then(function(){
+			return getFaculty();
+		}).then(function(faculty_list2){
+			if(resolved){ //You have this set to true fix it
+				for(let i in faculty_list2){
+					resolved_faculty_values[i] = faculty_list2[i].id;faculty_list2
+				}
+				resolved_faculty = resolved_faculty_values[Math.round(Math.random()*(resolved_faculty_values.length-1))];
+				issues = issues.concat(` ${resolved_faculty})`);
+			}
+			else{
+				issues = issues.concat(' null)');
+			}
+			return;
+		}).then(function(){
+			pool.getConnection(function(error, connection){
+				if(error)
+					return reject(error);
+				connection.query(issues, function(err, rows, fields){
+					connection.release();
+					console.log('---------------------------------------------------');
+					console.log(`Issue Description for Record ${i+1}: ${description}`);
+					console.log(`Issue Location for Record ${i+1}: ${location}`);
+					console.log(`Issue Verified for Record ${i+1}: ${verified}`);
+					console.log(`Issue Resolved for Record ${i+1}: ${resolved}`);
+					console.log(`Issue Submitted User Id for Record ${i+1}: ${submitted_user}`);
+					console.log(`Issue Verified Faculty User Id for Record ${i+1}: ${verified_faculty}`);
+					console.log(`Issue Resolved Faculty User Id for Record ${i+1}: ${resolved_faculty}`);
+					resolve(0);
+				});
 			});
-			*/
 		});
-	}
+	});
+}
+
+loopSeedUsers = function(count){
+	return new Promise(function(resolve, reject){
+		for(let i = 0; i < count; i++){
+			seedUsers(i).then(function(results){
+				console.log(`User Record ${i+1} Created!`);
+			}).catch(function(err){
+				console.log(err);
+			});
+		}
+		resolve();
+	});
+}
+
+loopSeedGarages = function(count){
+	return new Promise(function(resolve, reject){
+		for(let i = 0; i < count; i++){
+			seedGarages(i).then(function(results){
+				console.log(`Garage Record ${i+1} Created!`);
+			}).then(function(){
+				resolve();
+			});
+		}
+		resolve();
+	});
+}
+
+loopSeedEvents = function(count){
+	return new Promise(function(resolve, reject){
+		for(let i = 0; i < count; i++){
+			seedEvents(i).then(function(results){
+				console.log(`Event Record ${i+1} Created!`);
+			}).catch(function(err){
+				console.log(err);
+			});
+		}
+		resolve();
+	});
+}
+
+loopSeedIssues = function(count){
+	return new Promise(function(resolve, reject){
+		for(let i = 0; i < count; i++){
+			seedIssues(i).then(function(results){
+				console.log(`Issue Record ${i+1} Created!`);
+			}).catch(function(err){
+				console.log(err);
+			});
+		}
+		resolve();
+	});
+}
+
+seedDatabase = function(){
+	return new Promise(function(resolve, reject){
+		loopSeedUsers(usersCount).then(loopSeedGarages(garagesCount)).then(loopSeedEvents(eventsCount))
+		.then(loopSeedIssues(issuesCount)).then(function(){
+			resolve();
+		});
+	});
+}
+
+seedDatabase().then(function(){
+	console.log('Database has been Seeded!');
+}).catch(function(err){
+	console.log(err);
 });
-return;
