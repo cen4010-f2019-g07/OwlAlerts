fff3b4411d5bb9d2c4cb3db0cc0220830efe3d10
AkivaGreen <green.akiva@outlook.com>
2019-11-13 19:33:10 -0500

    Added basic back-end issue controller code

--- controllers/issues_controller.js
+++ controllers/issues_controller.js
@@ -40,11 +40,7 @@ function databaseQuery(query){
 //Home page for Issues.
 exports.index = function(req, res) {
 	let query = 'SELECT * FROM issues';
-	databaseQuery(query).then(function(results){
-		let data = [];
-		console.log(results);
-		return data;
-	}).then(function(data){
+	databaseQuery(query).then(function(data){
 		res.send('NOT IMPLEMENTED: Issue index');
 	}).catch(function(err){
 		console.log(err);
@@ -53,16 +49,27 @@ exports.index = function(req, res) {
 
 // Display list of all Issues.
 exports.issue_list = function(req, res) {
-  res.send('NOT IMPLEMENTED: Issue list');
+	let query = 'SELECT * FROM issues';
+	databaseQuery(query).then(function(data){
+		//data is an array of all issues
+		res.send('NOT IMPLEMENTED: Issue list');
+	}).catch(function(err){
+		console.log(err);
+	});
 };
 
 // Display detail page for a specific Issue.
 exports.issue_detail = function(req, res) {
 	let query = `SELECT * FROM issues WHERE id=${req.params.id}`;
 	databaseQuery(query).then(function(result){
-
-	})
-  res.send('NOT IMPLEMENTED: Issue detail: ' + req.params.id);
+		let data = result[0]; //The returned result is an array with one element
+		return data;
+	}).then(function(data){
+		//Data holds the information for the issue with the id param
+		res.send('NOT IMPLEMENTED: Issue detail: ' + req.params.id);
+	}).catch(function(err){
+		console.log(err);
+	});
 };
 
 // Display Issue create form on GET.
@@ -77,7 +84,16 @@ exports.issue_create_post = function(req, res) {
 
 // Display Issue delete form on GET.
 exports.issue_delete_get = function(req, res) {
-  res.send('NOT IMPLEMENTED: Issue delete GET');
+	let query = `SELECT * FROM issues WHERE id=${req.params.id}`;
+	databaseQuery(query).then(function(result){
+		let data = result[0]; //The returned result is an array with one element
+		return data;
+	}).then(function(data){
+		//Data holds the information for the issue with the id param
+		res.send('NOT IMPLEMENTED: Issue delete GET: ' + req.params.id);
+	}).catch(function(err){
+		console.log(err);
+	});
 };
 
 // Handle Issue delete on POST.
@@ -87,7 +103,16 @@ exports.issue_delete_post = function(req, res) {
 
 // Display Issue update form on GET.
 exports.issue_update_get = function(req, res) {
-  res.send('NOT IMPLEMENTED: Issue update GET');
+	let query = `SELECT * FROM issues WHERE id=${req.params.id}`;
+	databaseQuery(query).then(function(result){
+		let data = result[0]; //The returned result is an array with one element
+		return data;
+	}).then(function(data){
+		//Data holds the information for the issue with the id param
+		res.send('NOT IMPLEMENTED: Issue update GET: ' + req.params.id);
+	}).catch(function(err){
+		console.log(err);
+	});
 };
 
 // Handle Issue update on POST.
