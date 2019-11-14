f874fd5bf9f86432a1739cdcd47a359a1994697d
AkivaGreen <green.akiva@outlook.com>
2019-11-13 19:37:44 -0500

    Added basic back-end events controller code

--- controllers/events_controller.js
+++ controllers/events_controller.js
@@ -1,18 +1,74 @@
 var event = require('../models/event');
+const mysql = require('mysql');
+const production = process.env.production;
+const bodyParser = require('body-parser');
+
+if(production == true){
+	pool = mysql.createPool({
+		connectionLimit: 100,
+		host: 'localhost',
+		user: 'cen4010fal19_g07',
+		password: 'kJDrofNeU6',
+		database: 'cen4010fal19_g07',
+		multipleStatements: true
+	});
+}
+else{
+	pool = mysql.createPool({
+		connectionLimit: 100,
+		host: 'localhost',
+		user: 'user',
+		password: 'password',
+		database: 'owl_alerts',
+		multipleStatements: true
+	});
+}
+
+function databaseQuery(query){
+	return new Promise(function(resolve, reject){
+		pool.getConnection(function(error, connection){
+			connection.query(query, function(err, rows, fields){
+				connection.release();
+				if(err)
+					return reject(err);
+				resolve(rows);
+			});
+		});
+	});
+}
 
 // Home page for Events
 exports.index = function(req, res) {
-	res.send('NOT IMPLEMENTED: Event index');
+	let query = 'SELECT * FROM events';
+	databaseQuery(query).then(function(data){
+		res.send('NOT IMPLEMENTED: Event index');
+	}).catch(function(err){
+		console.log(err);
+	});
 };
 
 // Display list of all Events.
 exports.event_list = function(req, res) {
-  res.send('NOT IMPLEMENTED: Event list');
+	let query = 'SELECT * FROM events';
+	databaseQuery(query).then(function(data){
+		res.send('NOT IMPLEMENTED: Event list');
+	}).catch(function(err){
+		console.log(err);
+	});
 };
 
 // Display detail page for a specific Event.
 exports.event_detail = function(req, res) {
-  res.send('NOT IMPLEMENTED: Event detail: ' + req.params.id);
+	let query = `SELECT * FROM events WHERE id=${req.params.id}`;
+	databaseQuery(query).then(function(result){
+		let data = result[0]; //The returned result is an array with one element
+		return data;
+	}).then(function(data){
+		//Data holds the information for the issue with the id param
+		res.send('NOT IMPLEMENTED: Event detail: ' + req.params.id);
+	}).catch(function(err){
+		console.log(err);
+	});
 };
 
 // Display Event create form on GET.
@@ -27,7 +83,16 @@ exports.event_create_post = function(req, res) {
 
 // Display Event delete form on GET.
 exports.event_delete_get = function(req, res) {
-  res.send('NOT IMPLEMENTED: Event delete GET');
+	let query = `SELECT * FROM events WHERE id=${req.params.id}`;
+	databaseQuery(query).then(function(result){
+		let data = result[0]; //The returned result is an array with one element
+		return data;
+	}).then(function(data){
+		//Data holds the information for the issue with the id param
+		res.send('NOT IMPLEMENTED: Event delete GET: ' + req.params.id);
+	}).catch(function(err){
+		console.log(err);
+	});
 };
 
 // Handle Event delete on POST.
@@ -37,7 +102,16 @@ exports.event_delete_post = function(req, res) {
 
 // Display Event update form on GET.
 exports.event_update_get = function(req, res) {
-  res.send('NOT IMPLEMENTED: Event update GET');
+	let query = `SELECT * FROM events WHERE id=${req.params.id}`;
+	databaseQuery(query).then(function(result){
+		let data = result[0]; //The returned result is an array with one element
+		return data;
+	}).then(function(data){
+		//Data holds the information for the issue with the id param
+		res.send('NOT IMPLEMENTED: Evnet update GET: ' + req.params.id);
+	}).catch(function(err){
+		console.log(err);
+	});
 };
 
 // Handle Event update on POST.
