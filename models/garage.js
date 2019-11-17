c9176db16c3b74dbc492b722a867a8b7cc430240
AkivaGreen <green.akiva@outlook.com>
2019-11-16 20:04:34 -0500

    Model code for garages setup

--- models/garage.js
+++ models/garage.js
@@ -1,16 +1,94 @@
 var mysql = require('mysql');
-var garageModel;
-/*
-garageModel.create = function(){
+const production = process.env.production;
 
+function databaseQuery(query){
+	return new Promise(function(resolve, reject){
+		pool.getConnection(function(error, connection){
+			connection.query(query, function(err, rows, fields){
+				connection.release();
+				if(err)
+					return reject(err);
+				resolve(rows);
+			});
+		});
+	});
 }
 
-garageModel.update = function(){
+class Garage {
+	contructor(){;
+		if(production == true){
+			this.pool = mysql.createPool({
+				connectionLimit: 100,
+				host: 'localhost',
+				user: 'cen4010fal19_g07',
+				password: 'kJDrofNeU6',
+				database: 'cen4010fal19_g07',
+				multipleStatements: true
+			});
+		}
+		else{
+			this.pool = mysql.createPool({
+				connectionLimit: 100,
+				host: 'localhost',
+				user: 'user',
+				password: 'password',
+				database: 'owl_alerts',
+				multipleStatements: true
+			});
+		}
+	}
 
+	create(){ //Needs to be done
+		return new Promise(function(resolve, reject){
+			let query = '';
+			databaseQuery(query).then(function(result){
+				resolve(result);
+			}).catch(function(err){
+				console.log(err);
+			});
+		});
+	}
+
+	update(){
+		return new Promise(function(resolve, reject){
+
+		});
+	}
+
+	delete(id){
+		return new Promise(function(resolve, reject){
+			let query = `DELETE FROM garages WHERE id=${id}`;
+			databaseQuery(query).then(function(result){
+				resolve(result);
+			}).catch(function(err){
+				console.log(err);
+			});
+		});
+	}
+
+	all(){
+		return new Promise(function(resolve, reject){
+			let query = 'SELECT * FROM garages';
+			databaseQuery(query).then(function(result){
+				resolve(result);
+			}).catch(function(err){
+				console.log(err);
+			});
+		});
+	}
+
+	get(id){
+		return new Promise(function(resolve, reject){
+			let query = `SELECT * FROM garages WHERE id=\'${id}\'`;
+			databaseQuery(query).then(function(result){
+				resolve(result[0]);
+			}).catch(function(err){
+				console.log(err);
+			});
+		});
+	}
 }
 
-garageModel.delete = function(){
-	
-}
-*/
-module.exports = garageModel;
+var GarageModel = new Garage();
+
+module.exports = GarageModel;
