07c655e1fc7589ce6e5bc0fccdf54f7e1855a13b
Danica <dbrozows@fau.edu>
2019-11-17 10:03:49 -0500

    ok

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -6,4 +6,10 @@ var dashboard_controller = require('../controllers/dashboard_controller');
 //Home page for dashboard
 router.get('/show', dashboard_controller.show);
 
+//privacy policy page
+router.get('/showpp', dashboard_controller.showpp);
+
+//terms and conditions page
+router.get('/showtc', dashboard_controller.showtc);
+
 module.exports = router;
