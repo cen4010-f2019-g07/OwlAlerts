233dd3e900ff041b984a19f7d01f9543b8621588
Danica <dbrozows@fau.edu>
2019-11-17 10:15:39 -0500

    fixed routes and controller

--- routes/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-var index_controller = require('../controllers/index_controller');
-
-// GET home page.
-router.get('/', index_controller.index);
-
-module.exports = router;
