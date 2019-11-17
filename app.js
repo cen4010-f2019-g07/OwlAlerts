a9c4004c6df2a08e76d3156b47e40a3e6e446daa
AkivaGreen <green.akiva@outlook.com>
2019-11-16 20:30:03 -0500

    Refactored for pool being independent

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 "use strict";
 const express = require('express');
-const mysql = require('mysql');
+const pool = require('./lib/pool_db');
 const bodyParser = require('body-parser');
 const http = require('http');
 const session = require('express-session');
@@ -26,7 +26,6 @@ const passport = require('./config/passport');
 
 const app = express();
 const production = process.env.production || false;
-var pool;
 
 app.use(express.static('public'));
 app.use(express.static('node_modules'));
@@ -46,6 +45,16 @@ app.use('/dashboard', dashboardRouter);
 
 app.set('view engine', 'ejs');
 
+if(production){
+	http.createServer(app, function(req, res){
+		res.writeHead(200, {'Content-Type': 'text/plain'});
+		res.end("Hello Node.js\n");
+	}).listen(14250), () => console.log('Listening on port 14250...');
+}
+else{
+	app.listen(3000, () => console.log('Listening on port 3000...'));
+}
+
 passport.serializeUser(function(user, done){
 	done(null, user.id);
 });
@@ -61,46 +70,4 @@ passport.deserializeUser(function(id, done){
 			done(err, rows[0]);
 		});
 	});
-});
-
-if(production == true){
-	pool = mysql.createPool({
-		connectionLimit: 100,
-		host: 'localhost',
-		user: 'cen4010fal19_g07',
-		password: 'kJDrofNeU6',
-		database: 'cen4010fal19_g07',
-		multipleStatements: true
-	});
-	http.createServer(app, function(req, res){
-		res.writeHead(200, {'Content-Type': 'text/plain'});
-		res.end("Hello Node.js\n");
-	}).listen(14250), () => console.log('Listening on port 14250...');
-}
-else{
-	pool = mysql.createPool({
-		connectionLimit: 100,
-		host: 'localhost',
-		user: 'user',
-		password: 'password',
-		database: 'owl_alerts',
-		multipleStatements: true
-	});
-	app.listen(3000, () => console.log('Listening on port 3000...'));
-}
-
-pool.on('acquire', function(connection){
-	console.log('Connection %d acquired', connection.threadId);
-});
-
-pool.on('release', function(connection){
-	console.log('Connection %d released', connection.threadId);
-});
-
-pool.on('enqueue', function(){
-	console.log('Waiting for available connection slot');
-});
-
-pool.on('connection', function(connection){
-	connection.query('SET SESSION auto_increment_increment=1');
 });
