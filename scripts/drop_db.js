bc2352cb1c112b7b904c05a9a31b7f75b040e2fe
AkivaGreen <green.akiva@outlook.com>
2019-11-18 13:51:58 -0500

    Drop db script production ready

--- scripts/drop_db.js
+++ scripts/drop_db.js
@@ -1,11 +1,32 @@
 const mysql = require('mysql');
-const drop_db = 'DROP DATABASE owl_alerts';
+const drop_db;
+const production = process.env.production || false;
+var connection;
 
-var connection = mysql.createConnection({
-	host: 'localhost',
-	user: 'user',
-	password: 'password'
-});
+if(production){
+	connection = mysql.createConnection({
+		connectionLimit: 10,
+		host: 'localhost',
+		user: 'cen4010fal19_g07',
+		password: 'kJDrofNeU6',
+		database: 'cen4010fal19_g07',
+		multipleStatements: true
+	});
+	let drop_events = 'DROP TABLE events';
+	let drop_garages = 'DROP TABLE garages';
+	let drop_issues = 'DROP TABLE issues';
+	let drop_users = 'DROP TABLE users';
+	drop_db = [drop_events, drop_garages, drop_issues, drop_users];
+}
+else{
+	connection = mysql.createConnection({
+		host: 'localhost',
+		user: 'user',
+		password: 'password'
+	});
+	drop_db = 'DROP DATABASE owl_alerts';
+}
+
 
 function databaseQuery(queryString){
 	return new Promise(function(resolve, reject){
