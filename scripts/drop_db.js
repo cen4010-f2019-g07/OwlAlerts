dc3bf2e281d05656eaf1a863575f1cf8f355e40b
AkivaGreen <green.akiva@outlook.com>
2019-11-18 14:06:02 -0500

    Drop db production ready

--- scripts/drop_db.js
+++ scripts/drop_db.js
@@ -1,6 +1,6 @@
 "use strict";
 const mysql = require('mysql');
-var drop_db;
+
 const production = process.env.production || false;
 var connection;
 
@@ -13,11 +13,6 @@ if(production){
 		database: 'cen4010fal19_g07',
 		multipleStatements: true
 	});
-	let drop_events = 'DROP TABLE events';
-	let drop_garages = 'DROP TABLE garages';
-	let drop_issues = 'DROP TABLE issues';
-	let drop_users = 'DROP TABLE users';
-	drop_db = [drop_events, drop_garages, drop_issues, drop_users];
 }
 else{
 	connection = mysql.createConnection({
@@ -25,7 +20,6 @@ else{
 		user: 'user',
 		password: 'password'
 	});
-	drop_db = 'DROP DATABASE owl_alerts';
 }
 
 
@@ -39,10 +33,29 @@ function databaseQuery(queryString){
 	});
 }
 
-databaseQuery(drop_db).then(function(){
-	console.log('Database Deleted!');
-	connection.end();
-	return;
-}).catch(function(err){
-	console.log(err);
-});
+if(production){
+	let drop_events = 'DROP TABLE events';
+	let drop_garages = 'DROP TABLE garages';
+	let drop_issues = 'DROP TABLE issues';
+	let drop_users = 'DROP TABLE users';
+	let drop_images = 'DROP TABLE images';
+	databaseQuery(drop_events).then(databaseQuery(drop_garages))
+	.then(databaseQuery(drop_issues)).then(databaseQuery(drop_users))
+	.then(databaseQuery(drop_images)).then(function(){
+		console.log('Tables Dropped!');
+		connection.end();
+		return;
+	}).catch(function(err){
+		console.log(err);
+	});
+}
+else{
+	let drop_db = 'DROP DATABASE owl_alerts';
+	databaseQuery(drop_db).then(function(){
+		console.log('Database Deleted!');
+		connection.end();
+		return;
+	}).catch(function(err){
+		console.log(err);
+	});
+}
