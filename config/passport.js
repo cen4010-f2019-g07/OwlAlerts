c942682c3c16158fbc65cca065865a6bfef8f7be
AkivaGreen <green.akiva@outlook.com>
2019-11-18 18:48:21 -0500

    Abstracted out model code

--- config/passport.js
+++ config/passport.js
@@ -1,25 +1,14 @@
 "use strict";
-const pool = require('../lib/pool_db');
+var UserModel = require('../models/user');
 var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
 
-function databaseQuery(query){
-	return new Promise(function(resolve, reject){
-		pool.query(query, function(err, rows, fields){
-			if(err)
-				return reject(err);
-			resolve(rows);
-		});
-	});
-}
-
 passport.use(new LocalStrategy(function(username, password, done){
-	const userQuery = `SELECT * FROM users WHERE email=\'${username}\' AND password=\'${password}\'`;
-	databaseQuery(userQuery).then(function(result){
-		if(result.length == 0){
+	UserModel.checkLogin(username, password).then(function(user){
+		if(!user){
 			return done(null, false, {message: 'Incorrect Email or Password!'});
 		}
-		return done(null, result[0]);
+		return done(null, user);
 	}).catch(function(err){
 		return done(err);
 	});
