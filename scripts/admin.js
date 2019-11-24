758767adc5827e4e1befcd5d8fa00c297afffd08
AkivaGreen <green.akiva@outlook.com>
2019-11-24 14:17:05 -0500

    Updated admin creation script

--- scripts/admin.js
+++ scripts/admin.js
@@ -1,28 +1,17 @@
 "use strict";
-const mysql = require('mysql');
+const pool = require('../lib/pool_db');
 const faker = require('faker');
-const production = process.env.production || false;
-var pool;
+const bcrypt = require('bcryptjs');
+const saltRounds = 10;
 
-if(production){
-	pool = mysql.createPool({
-		connectionLimit: 10,
-		host: 'localhost',
-		user: 'cen4010fal19_g07',
-		password: 'kJDrofNeU6',
-		database: 'cen4010fal19_g07'
+function databaseQuery(query){
+	return new Promise(function(resolve, reject){
+		pool.query(query, function(err, rows, fields){
+			if(err)
+				return reject(err);
+			resolve(rows);
+		});
 	});
-	console.log('Connected to Production Database!');
-}
-else{
-	pool = mysql.createPool({
-		connectionLimit: 10,
-		host: 'localhost',
-		user: 'user',
-		password: 'password',
-		database: 'owl_alerts'
-	});
-	console.log('Connected to Development Database!');
 }
 
 function seedAdmin(){
@@ -31,22 +20,21 @@ function seedAdmin(){
 		let lastname = 'Admin';
 		let email = 'admin@fau.edu';
 		let password = 'password';
-		let adminQuery = `INSERT INTO users(firstname, lastname, email, password, admin) \
-		VALUE(\'${firstname}\', \'${lastname}\', \'${email}\', \'${password}\', true)`;
-		pool.getConnection(function(error, connection){
-			if(error)
-				return reject(error);
-			connection.query(adminQuery, function(err, rows, fields){
-				if(err)
-					return reject(err);
-				connection.release();
-				console.log('---------------------------------------------------');
-				console.log(`Admin User First Name: ${firstname}`);
-				console.log(`Admin User Last Name: ${lastname}`);
-				console.log(`Admin User Email Address: ${email}`);
-				console.log(`Admin User Password: ${password}`);
-				resolve(rows);
+		bcrypt.hash(password, saltRounds).then(function(hash){
+			let adminQuery = `INSERT INTO users(firstname, lastname, email, password, admin) \
+			VALUE(\'${firstname}\', \'${lastname}\', \'${email}\', \'${hash}\', true)`;
+			databaseQuery(adminQuery).then(function(result){
+					console.log('---------------------------------------------------');
+					console.log(`Admin User First Name: ${firstname}`);
+					console.log(`Admin User Last Name: ${lastname}`);
+					console.log(`Admin User Email Address: ${email}`);
+					console.log(`Admin User Password: ${password}`);
+					resolve(result);
+			}).catch(function(err){
+				console.log(err);
 			});
+		}).catch(function(err){
+			console.log(err);
 		});
 	});
 }
