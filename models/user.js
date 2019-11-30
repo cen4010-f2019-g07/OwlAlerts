3d974430aa691e2cabc8ac346eba9b3dff24076d
AkivaGreen <green.akiva@outlook.com>
2019-11-30 12:23:02 -0500

    Made update if statements short circuit

--- models/user.js
+++ models/user.js
@@ -39,19 +39,19 @@ class User {
 			let getUserQuery = `SELECT * FROM users WHERE id='${attr['id']}'`;
 			databaseQuery(getUserQuery).then(function(result){
 				let user = result[0];
-				if(attr['residency'] != user.residency && attr['residency'] != null){
+				if(attr['residency'] != null && attr['residency'] != user.residency){
 					let residencyQuery = `UPDATE users SET residency='${attr['residency']}' WHERE id='${attr['id']}'`;
 					databaseQuery(residencyQuery).catch(function(err){
 						console.log(err);
 					});
 				}
-				if(attr['faculty'] != user.faculty  && attr['faculty'] != null){
+				if(attr['faculty'] != null && attr['faculty'] != user.faculty){
 					let facultyQuery = `UPDATE users SET faculty=${attr['faculty']} WHERE id='${attr['id']}'`;
 					databaseQuery(facultyQuery).catch(function(err){
 						console.log(err);
 					});
 				}
-				if(attr['housing_status'] != user.housing_status  && attr['housing_status'] != null){
+				if(attr['housing_status'] != null && attr['housing_status'] != user.housing_status){
 					//Make sure that both dormer and commuter can't be true
 					let housingStatusQuery = `UPDATE users SET housing_status='${attr['housing_status']}' \
 					WHERE id='${attr['id']}'`;
@@ -59,55 +59,55 @@ class User {
 						console.log(err);
 					});
 				}
-				if(attr['building'] != user.building  && attr['building'] != null){
+				if(attr['building'] != null && attr['building'] != user.building){
 					let buildingQuery = `UPDATE users SET building='${attr['building']}' WHERE id='${attr['id']}'`;
 					databaseQuery(buildingQuery).catch(function(err){
 						console.log(err);
 					});
 				}
-				if(attr['room_number'] != user.room_number  && attr['room_number'] != null){
+				if(attr['room_number'] != null && attr['room_number'] != user.room_number){
 					let roomQuery = `UPDATE users SET room_number='${attr['room_number']}' WHERE id='${attr['id']}'`;
 					databaseQuery(roomQuery).catch(function(err){
 						console.log(err);
 					});
 				}
-				if(attr['phone_number'] != user.phone_number  && attr['phone_number'] != null){
+				if(attr['phone_number'] != null && attr['phone_number'] != user.phone_number){
 					let phoneQuery = `UPDATE users SET phone_number='${attr['phone_number']}' WHERE id='${attr['id']}'`;
 					databaseQuery(phoneQuery).catch(function(err){
 						console.log(err);
 					});
 				}
-				if(attr['street'] != user.street  && attr['street'] != null){
+				if(attr['street'] != null && attr['street'] != user.street){
 					let streetQuery = `UPDATE users SET street='${attr['street']}' WHERE id='${attr['id']}'`;
 					databaseQuery(streetQuery).catch(function(err){
 						console.log(err);
 					});
 				}
-				if(attr['city'] != user.city  && attr['city'] != null){
+				if(attr['city'] != null && attr['city'] != user.city){
 					let cityQuery = `UPDATE users SET city='${attr['city']}' WHERE id='${attr['id']}'`;
 					databaseQuery(cityQuery).catch(function(err){
 						console.log(err);
 					});
 				}
-				if(attr['state'] != user.state  && attr['state'] != null){
+				if(attr['state'] != null && attr['state'] != user.state){
 					let stateQuery = `UPDATE users SET state='${attr['state']}' WHERE id='${attr['id']}'`;
 					databaseQuery(stateQuery).catch(function(err){
 						console.log(err);
 					});
 				}
-				if(attr['zip'] != user.zip  && attr['zip'] != null){
+				if(attr['zip'] != null && attr['zip'] != user.zip){
 					let zipQuery = `UPDATE users SET zip='${attr['zip']}' WHERE id='${attr['id']}'`;
 					databaseQuery(zipQuery).catch(function(err){
 						console.log(err);
 					});
 				}
-				if(attr['country'] != user.country  && attr['country'] != null){
+				if(attr['country'] != null && attr['country'] != user.country){
 					let countryQuery = `UPDATE users SET country='${attr['country']}' WHERE id='${attr['id']}'`;
 					databaseQuery(countryQuery).catch(function(err){
 						console.log(err);
 					});
 				}
-				if(attr['email'] != user.email  && attr['email'] != null){
+				if(attr['email'] != null && attr['email'] != user.email){
 					let emailQuery = `UPDATE users SET email='${attr['email']}' WHERE id='${attr['id']}'`;
 					databaseQuery(emailQuery).catch(function(err){
 						console.log(err);
@@ -123,19 +123,19 @@ class User {
 						});
 					}
 				})
-				if(attr['firstname'] != user.firstname  && attr['firstname'] != null){
+				if(attr['firstname'] != null && attr['firstname'] != user.firstname){
 					let firstnameQuery = `UPDATE users SET firstname='${attr['firstname']}' WHERE id='${attr['id']}'`;
 					databaseQuery(firstnameQuery).catch(function(err){
 						console.log(err);
 					});
 				}
-				if(attr['lastname'] != user.lastname  && attr['lastname'] != null){
+				if(attr['lastname'] != null && attr['lastname'] != user.lastname){
 					let lastnameQuery = `UPDATE users SET lastname='${attr['lastname']}' WHERE id='${attr['id']}'`;
 					databaseQuery(lastnameQuery).catch(function(err){
 						console.log(err);
 					});
 				}
-				if(attr['image_id'] != user.image_id  && attr['image_id'] != null){
+				if(attr['image_id'] != null && attr['image_id'] != user.image_id){
 					let imageQuery = `UPDATE users SET image_id='${attr['image_id']}' WHERE id='${attr['id']}'`;
 					databaseQuery(imageQuery).catch(function(err){
 						console.log(err);
