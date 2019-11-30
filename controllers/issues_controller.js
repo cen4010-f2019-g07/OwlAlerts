2b2feb2cb3d2050d7c414f197b501cc98d0c04e2
AkivaGreen <green.akiva@outlook.com>
2019-11-29 19:12:26 -0500

    Deleted UserModel variable

--- controllers/issues_controller.js
+++ controllers/issues_controller.js
@@ -1,6 +1,5 @@
 "use strict";
 var IssueModel = require('../models/issue');
-var UserModel = require('../models/user');
 const paginate = require('express-paginate');
 const ImageModel = require('../models/image');
 
@@ -19,7 +18,6 @@ exports.index = function(req, res) {
     {
       sessionUser: req.user,
       issues: data,
-      userQuery: UserModel,
       message: req.flash()
     });
 	}).catch(function(err){
