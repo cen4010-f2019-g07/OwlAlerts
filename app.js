d285b51e3c06d4d3d8268cdfa8a74be573dea9b6
AkivaGreen <green.akiva@outlook.com>
2019-11-29 20:44:53 -0500

    Fixed syntax error

--- app.js
+++ app.js
@@ -94,7 +94,7 @@ cron.schedule('00 00 00 * * 0-6', function(){
 //Monday - Friday
 cron.schedule('00 00 4-11 * * 1-5', function(){
 	GarageModel.all().then(function(garages){
-		let free_spots = (garages[i].total_spots - ((garages[i].total_spots)/9))
+		let free_spots = (garages[i].total_spots - ((garages[i].total_spots)/9));
 		for(var i in garages){
 			let attr = {
 				id: garages[i].id,
@@ -133,7 +133,7 @@ cron.schedule('00 00 12 * * 0-6', function(){
 //Monday - Friday
 cron.schedule('00 00 16-23 * * 1-5', function(){
 	GarageModel.all().then(function(garages){
-		let free_spots = (garages[i].free_spots + ((garages[i].total_spots)/9))
+		let free_spots = (garages[i].free_spots + ((garages[i].total_spots)/9));
 		for(var i in garages){
 			let attr = {
 				id: garages[i].id,
