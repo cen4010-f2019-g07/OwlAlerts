1d849d8b35c03a0d5b02c2835a9ef169d5b1d4c1
AkivaGreen <green.akiva@outlook.com>
2019-12-07 18:54:08 -0500

    Use of faker

--- test/model/user_spec.js
+++ test/model/user_spec.js
@@ -3,6 +3,7 @@ process.env.test = true;
 const assert = require('chai').assert;
 const expect = require('chai').expect;
 const should = require('chai').should();
+const faker = require('faker');
 const pool = require('../../lib/pool_db');
 const bcrypt = require('bcryptjs');
 const saltRounds = 10;
@@ -10,6 +11,65 @@ const UserModel = require('../../models/user');
 
 describe('User Model', function(){
 	let fakeUser = {};
+	let residency_values = ['Out-State', 'In-State'];
+	let housing_status_values = ['Commuter', 'Dormer'];
+	let building_values = ['IVAS', 'IVAN', 'UVA 57', 'UVA 58', 'UVA 59', 'UVA 60', 
+		'UVA 61', 'PAR', 'HPT', 'GPT', 'IRT'];
+	let counter = 0;
+	let uva_rooms = [];
+	let format_j;
+	for(let j = 0; j < 2; j++){
+		for(let k = 0; k < 30; k++){
+			format_j = ('0' + k).slice(-2);
+			uva_rooms[counter] = `${j+1}${format_j}`;
+			counter++;
+		}
+	}
+	let iva_rooms = [];
+	counter = 0;
+	for(let j = 0; j < 7; j++){
+		for(let k = 0; k < 70; k++){
+			format_j = ('0' + k).slice(-2);
+			iva_rooms[counter] = `${j+1}${format_j}`;
+			counter++;
+		}
+	}
+	let par_rooms = [];
+	counter = 0;
+	for(let j = 0; j < 5; j++){
+		for(let k = 0; k < 30; k++){
+			format_j = ('0' + k).slice(-2);
+			par_rooms[counter] = `${j+1}${format_j}`;
+			counter++
+		}
+	}
+	let hpt_rooms = [];
+	counter = 0;
+	for(let j = 0; j < 7; j++){419
+		for(let k = 0; k < 60; k++){
+			format_j = ('0' + k).slice(-2);
+			hpt_rooms[counter] = `${j+1}${format_j}`;
+			counter++
+		}
+	}
+	let gpt_rooms = [];
+	counter = 0;
+	for(let j = 0; j < 7; j++){
+		for(let k = 0; k < 60; k++){
+			format_j = ('0' + k).slice(-2);
+			gpt_rooms[counter] = `${j+1}${format_j}`;
+			counter++
+		}
+	}
+	let irt_rooms = [];
+	counter = 0;
+	for(let j = 0; j < 7; j++){
+		for(let k = 0; k < 60; k++){
+			format_j = ('0' + k).slice(-2);
+			irt_rooms[counter] = `${j+1}${format_j}`;
+			counter++
+		}
+	}
 
 	after(function(){
 		pool.end();
@@ -17,10 +77,10 @@ describe('User Model', function(){
 	});
 
 	describe('create function', function(){
-		fakeUser.firstname = 'First';
-		fakeUser.lastname = 'Last';
-		fakeUser.email = 'firstlast@email.com';
-		fakeUser.password = 'password';
+		fakeUser.firstname = faker.name.firstName()
+		fakeUser.lastname = faker.name.lastName();
+		fakeUser.email = faker.internet.password();
+		fakeUser.password = faker.internet.password();
 		it('should affect one row', function(done){
 			UserModel.create(fakeUser.firstname, fakeUser.lastname, fakeUser.email, fakeUser.password).then(function(result){
 				fakeUser.id = result.insertId;
@@ -33,10 +93,47 @@ describe('User Model', function(){
 		});
 	});
 
-	describe('update function', function(){
-		fakeUser.residency = 'In-State';
-		fakeUser.building = 'IVAN';
-		fakeUser.room_number = '203A';
+	describe('update function - Dormer', function(){
+		fakeUser.housing_status = 'Dormer';
+		fakeUser.residency = residency_values[Math.round(Math.random())];
+		fakeUser.building = building_values[Math.round(Math.random()*10)];
+		switch(fakeUser.building){
+			case 'PAR':
+				fakeUser.room_number = par_rooms[Math.round(Math.random()*149)]
+				break;
+			case 'UVA 57':
+				fakeUser.room_number = uva_rooms[Math.round(Math.random()*59)]
+				break;
+			case 'UVA 58':
+				fakeUser.room_number = uva_rooms[Math.round(Math.random()*59)]
+				break;
+			case 'UVA 59':
+				fakeUser.room_number = uva_rooms[Math.round(Math.random()*59)]
+				break;
+			case 'UVA 60':
+				fakeUser.room_number = uva_rooms[Math.round(Math.random()*59)]
+				break;
+			case 'UVA 61':
+				fakeUser.room_number = uva_rooms[Math.round(Math.random()*59)]
+				break;
+			case 'IRT':
+				fakeUser.room_number = irt_rooms[Math.round(Math.random()*419)]
+				break;
+			case 'HPT':
+				fakeUser.room_number = hpt_rooms[Math.round(Math.random()*419)]
+				break;
+			case 'GPT':
+				fakeUser.room_number = gpt_rooms[Math.round(Math.random()*419)]
+				break;
+			case 'IVAS':
+				fakeUser.room_number = iva_rooms[Math.round(Math.random()*489)]
+				break;
+			case 'IVAN':
+				fakeUser.room_number = iva_rooms[Math.round(Math.random()*489)]
+				break;
+			default:
+				console.log('Room Switch Statment Not Working!');
+		}
 		it('should return a 1', function(done){
 			UserModel.update(fakeUser).then(function(result){
 				assert.equal(result, 1);
