76f79551e70b6206479c4cdce90201828b71e6b6
AkivaGreen <green.akiva@outlook.com>
2019-12-07 19:00:23 -0500

    Added basic model code

---
+++ test/model/event_spec.js
@@ -0,0 +1,24 @@
+"use strict";
+process.env.test = true;
+const assert = require('chai').assert;
+const expect = require('chai').expect;
+const should = require('chai').should();
+const faker = require('faker');
+const pool = require('../../lib/pool_db');
+const EventModel = require('../../models/event');
+
+describe('Event Model', function(){
+	let fakeEvent = {};
+
+	describe('create function', function(){
+		it('should return 1 row affected');
+	});
+
+	describe('update function', function(){
+		it('should return 1');
+	});
+
+	describe('delete function', function(){
+		it('should return 1 row affected');
+	});
+})
